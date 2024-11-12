ad6ee000b214948291fa428aff1a6c1f6066e1e4
Ocknon <ockn0n@hotmail.com>
2024-11-12 04:59:03 -0800

    Updated original plugin to work with new BepInEx
    Updates from older version to 6.0.*

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using System.Reflection;
+using System.IO;
+using BepInEx.Unity.IL2CPP;
+
+namespace RF5_FastMining
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess(GAME_PROCESS)]
+    public class Main : BasePlugin
+    {
+        #region PluginInfo
+        private const string GUID = "0328C54A-E525-486C-9D69-AD02526598F8";
+        private const string NAME = "RF5_FastMining";
+        private const string VERSION = "1.0.1";
+        private const string GAME_PROCESS = "Rune Factory 5.exe";
+        #endregion
+
+        public static ManualLogSource Logger;
+        private static string FILENAME = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + NAME + ".ini";
+        public static new IniParser Config;
+
+        public override void Load()
+        {
+            Logger = Log;
+            Config = new IniParser(FILENAME);
+            Harmony.CreateAndPatchAll(typeof(MiningPointDoChop));
+            Harmony.CreateAndPatchAll(typeof(MiningPointDoBreak));
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ MiningPointPatch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_FastMining
+{
+    // 锤石头
+    [HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoBreak))]
+    public class MiningPointDoBreak
+    {
+        private static int counter = 0;
+
+        static void Prefix()
+        {
+            ++counter;
+        }
+
+        static void Postfix(HumanController humanController, MiningPoint __instance)
+        {
+            if (counter == 1)
+                Main.Logger.LogDebug(string.Format("MiningPoint.DoBreak {0} {1} {2}", __instance.mineType, __instance.mineTypeDataTable.HP, __instance.cropDataTable.CropHP));
+
+            if (__instance.mineType == MineTypeID.Mine_Runecrystal ||
+                counter >= Main.Config.GetInt("FastMining", "BreakCount", 9))
+            {
+                --counter;
+                return;
+            }
+
+            if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0)
+                __instance.DoBreak(humanController);    // 递归调用
+
+            --counter;
+        }
+    }
+
+    // 劈树桩
+    [HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoChop))]
+    public class MiningPointDoChop
+    {
+        private static int counter = 0;
+
+        static void Prefix()
+        {
+            ++counter;
+        }
+
+        static void Postfix(HumanController humanController, MiningPoint __instance)
+        {
+            if (counter == 1)
+                Main.Logger.LogDebug(string.Format("MiningPoint.DoChop {0} {1} {2}", __instance.mineType, __instance.mineTypeDataTable.HP, __instance.cropDataTable.CropHP));
+
+            if (__instance.mineType == MineTypeID.Mine_Flower ||
+                counter >= Main.Config.GetInt("FastMining", "ChopCount", 9))
+            {
+                --counter;
+                return;
+            }
+
+            if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0)
+                __instance.DoChop(humanController);     // 递归调用
+
+            --counter;
+        }
+    }
+}
