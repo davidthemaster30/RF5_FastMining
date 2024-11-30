c4e8522289bead3a2b057df5c2ad1c9fca9cdd65
David <davidthemaster30@hotmail.com>
2024-11-29 22:25:14 -0500

    implicit usings + file scoped namespaces

--- IniParser.cs
+++ IniParser.cs
@@ -1,146 +1,140 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.Text;
 
-namespace RF5_FastMining
+namespace RF5_FastMining;
+
+public class IniParser
 {
-    public class IniParser
+    class TypeSet
     {
-        class TypeSet
+        public bool boolValue;
+        public int intValue;
+        public float floatValue;
+        public string stringValue;
+
+        public TypeSet(bool boolValue)
         {
-            public bool boolValue;
-            public int intValue;
-            public float floatValue;
-            public string stringValue;
-
-            public TypeSet(bool boolValue)
-            {
-                this.boolValue = boolValue;
-                intValue = boolValue ? 1 : 0;
-                floatValue = boolValue ? 1.0f : 0.0f;
-                stringValue = boolValue ? "true" : "false";
-            }
-
-            public TypeSet(int intValue)
-            {
-                boolValue = intValue > 0;
-                this.intValue = intValue;
-                floatValue = intValue;
-                stringValue = intValue.ToString();
-            }
-
-            public TypeSet(float floatValue)
-            {
-                boolValue = floatValue > 0;
-                intValue = (int)floatValue;
-                this.floatValue = floatValue;
-                stringValue = floatValue.ToString();
-            }
-
-            public TypeSet(string stringValue)
-            {
-                boolValue = stringValue.Length > 0;
-                int.TryParse(stringValue, out intValue);
-                float.TryParse(stringValue, out floatValue);
-                this.stringValue = stringValue;
-            }
+            this.boolValue = boolValue;
+            intValue = boolValue ? 1 : 0;
+            floatValue = boolValue ? 1.0f : 0.0f;
+            stringValue = boolValue ? "true" : "false";
         }
 
-        private Dictionary<string, Dictionary<string, TypeSet>> data = new Dictionary<string, Dictionary<string, TypeSet>>();
-
-        public IniParser()
+        public TypeSet(int intValue)
         {
+            boolValue = intValue > 0;
+            this.intValue = intValue;
+            floatValue = intValue;
+            stringValue = intValue.ToString();
         }
 
-        public IniParser(string fileName)
+        public TypeSet(float floatValue)
         {
-            string line = "";
-            string category = "";
-            using (StreamReader file = new StreamReader(fileName, Encoding.GetEncoding("utf-8")))
+            boolValue = floatValue > 0;
+            intValue = (int)floatValue;
+            this.floatValue = floatValue;
+            stringValue = floatValue.ToString();
+        }
+
+        public TypeSet(string stringValue)
+        {
+            boolValue = stringValue.Length > 0;
+            int.TryParse(stringValue, out intValue);
+            float.TryParse(stringValue, out floatValue);
+            this.stringValue = stringValue;
+        }
+    }
+
+    private Dictionary<string, Dictionary<string, TypeSet>> data = new Dictionary<string, Dictionary<string, TypeSet>>();
+
+    public IniParser()
+    {
+    }
+
+    public IniParser(string fileName)
+    {
+        string line = "";
+        string category = "";
+        using (StreamReader file = new StreamReader(fileName, Encoding.GetEncoding("utf-8")))
+        {
+            while ((line = file.ReadLine()) != null)
             {
-                while ((line = file.ReadLine()) != null)
+                line = line.Trim();
+                int delimiter = line.IndexOf(';');
+                if (delimiter > -1)
+                    line = line.Substring(0, delimiter).Trim();
+                if (line.Length <= 0)
+                    continue;
+
+                if (line.StartsWith("[") && line.EndsWith("]"))
                 {
-                    line = line.Trim();
-                    int delimiter = line.IndexOf(';');
+                    category = line.Substring(1, line.Length - 2);
+                    //Main.Logger.LogInfo(string.Format("category {0}", category));
+                }
+                else
+                {
+                    delimiter = line.IndexOf('=');
                     if (delimiter > -1)
-                        line = line.Substring(0, delimiter).Trim();
-                    if (line.Length <= 0)
-                        continue;
-
-                    if (line.StartsWith("[") && line.EndsWith("]"))
                     {
-                        category = line.Substring(1, line.Length - 2);
-                        //Main.Logger.LogInfo(string.Format("category {0}", category));
-                    }
-                    else
-                    {
-                        delimiter = line.IndexOf('=');
-                        if (delimiter > -1)
-                        {
-                            string key = line.Substring(0, delimiter).Trim();
-                            string value = line.Substring(delimiter + 1);
-                            //Main.Logger.LogInfo(string.Format("category {0} key {1} value {2}", category, key, value));
+                        string key = line.Substring(0, delimiter).Trim();
+                        string value = line.Substring(delimiter + 1);
+                        //Main.Logger.LogInfo(string.Format("category {0} key {1} value {2}", category, key, value));
 
-                            if (!data.ContainsKey(category))
-                                data.Add(category, new Dictionary<string, TypeSet>());
-                            if (!data[category].ContainsKey(key))
-                                data[category].Add(key, ParseValue(value));
-                        }
+                        if (!data.ContainsKey(category))
+                            data.Add(category, new Dictionary<string, TypeSet>());
+                        if (!data[category].ContainsKey(key))
+                            data[category].Add(key, ParseValue(value));
                     }
                 }
             }
         }
+    }
 
-        private TypeSet ParseValue(string value)
-        {
-            if (value.Length <= 0)
-                return null;
+    private TypeSet ParseValue(string value)
+    {
+        if (value.Length <= 0)
+            return null;
 
-            if (string.Compare(value, "true", true) == 0)
-                return new TypeSet(true);
-            if (string.Compare(value, "false", true) == 0)
-                return new TypeSet(false);
+        if (string.Compare(value, "true", true) == 0)
+            return new TypeSet(true);
+        if (string.Compare(value, "false", true) == 0)
+            return new TypeSet(false);
 
-            float fvalue;
-            if (float.TryParse(value, out fvalue))
-                return new TypeSet(fvalue);
+        float fvalue;
+        if (float.TryParse(value, out fvalue))
+            return new TypeSet(fvalue);
 
-            int ivalue;
-            if (int.TryParse(value, out ivalue))
-                return new TypeSet(ivalue);
+        int ivalue;
+        if (int.TryParse(value, out ivalue))
+            return new TypeSet(ivalue);
 
-            return new TypeSet(value);
-        }
+        return new TypeSet(value);
+    }
 
-        public int GetInt(string category, string name, int defaultValue)
-        {
-            if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
-                return defaultValue;
-            return data[category][name].intValue;
-        }
+    public int GetInt(string category, string name, int defaultValue)
+    {
+        if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
+            return defaultValue;
+        return data[category][name].intValue;
+    }
 
-        public float GetFloat(string category, string name, float defaultValue)
-        {
-            if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
-                return defaultValue;
-            return data[category][name].floatValue;
-        }
+    public float GetFloat(string category, string name, float defaultValue)
+    {
+        if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
+            return defaultValue;
+        return data[category][name].floatValue;
+    }
 
-        public bool GetBool(string category, string name, bool defaultValue)
-        {
-            if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
-                return defaultValue;
-            return data[category][name].boolValue;
-        }
+    public bool GetBool(string category, string name, bool defaultValue)
+    {
+        if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
+            return defaultValue;
+        return data[category][name].boolValue;
+    }
 
-        public string GetString(string category, string name, string defaultValue)
-        {
-            if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
-                return defaultValue;
-            return data[category][name].stringValue;
-        }
+    public string GetString(string category, string name, string defaultValue)
+    {
+        if (!data.ContainsKey(category) || !data[category].ContainsKey(name))
+            return defaultValue;
+        return data[category][name].stringValue;
     }
 }
--- MiningPointPatch.cs
+++ MiningPointPatch.cs
@@ -1,69 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 
-namespace RF5_FastMining
+namespace RF5_FastMining;
+
+[HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoBreak))]
+public class MiningPointDoBreak
 {
-    // 锤石头
-    [HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoBreak))]
-    public class MiningPointDoBreak
+    private static int counter = 0;
+
+    static void Prefix()
     {
-        private static int counter = 0;
-
-        static void Prefix()
-        {
-            ++counter;
-        }
-
-        static void Postfix(HumanController humanController, MiningPoint __instance)
-        {
-            if (counter == 1)
-                Main.Logger.LogDebug(string.Format("MiningPoint.DoBreak {0} {1} {2}", __instance.mineType, __instance.mineTypeDataTable.HP, __instance.cropDataTable.CropHP));
-
-            if (__instance.mineType == MineTypeID.Mine_Runecrystal ||
-                counter >= Main.Config.GetInt("FastMining", "BreakCount", 9))
-            {
-                --counter;
-                return;
-            }
-
-            if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0)
-                __instance.DoBreak(humanController);    // 递归调用
-
-            --counter;
-        }
+        ++counter;
     }
 
-    // 劈树桩
-    [HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoChop))]
-    public class MiningPointDoChop
+    static void Postfix(HumanController humanController, MiningPoint __instance)
     {
-        private static int counter = 0;
-
-        static void Prefix()
-        {
-            ++counter;
+        if (counter == 1){
+            Main.Logger.LogDebug($"MiningPoint.DoBreak {__instance.mineType} {__instance.mineTypeDataTable.HP} {__instance.cropDataTable.CropHP}");
         }
 
-        static void Postfix(HumanController humanController, MiningPoint __instance)
+        if (__instance.mineType == MineTypeID.Mine_Runecrystal ||
+            counter >= Main.Config.GetInt("FastMining", "BreakCount", 9))
         {
-            if (counter == 1)
-                Main.Logger.LogDebug(string.Format("MiningPoint.DoChop {0} {1} {2}", __instance.mineType, __instance.mineTypeDataTable.HP, __instance.cropDataTable.CropHP));
-
-            if (__instance.mineType == MineTypeID.Mine_Flower ||
-                counter >= Main.Config.GetInt("FastMining", "ChopCount", 9))
-            {
-                --counter;
-                return;
-            }
-
-            if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0)
-                __instance.DoChop(humanController);     // 递归调用
-
             --counter;
+            return;
         }
+
+        if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0){
+            __instance.DoBreak(humanController);
+        }
+
+        --counter;
+    }
+}
+
+[HarmonyPatch(typeof(MiningPoint), nameof(MiningPoint.DoChop))]
+public class MiningPointDoChop
+{
+    private static int counter = 0;
+
+    static void Prefix()
+    {
+        ++counter;
+    }
+
+    static void Postfix(HumanController humanController, MiningPoint __instance)
+    {
+        if (counter == 1){
+            Main.Logger.LogDebug($"MiningPoint.DoChop {__instance.mineType} {__instance.mineTypeDataTable.HP} {__instance.cropDataTable.CropHP}");
+        }
+
+        if (__instance.mineType == MineTypeID.Mine_Flower ||
+            counter >= Main.Config.GetInt("FastMining", "ChopCount", 9))
+        {
+            --counter;
+            return;
+        }
+
+        if (__instance.mineTypeDataTable.HP > 0 || __instance.cropDataTable.CropHP > 0){
+            __instance.DoChop(humanController);
+        }
+
+        --counter;
     }
 }
